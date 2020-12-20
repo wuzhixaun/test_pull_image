d3d5fe32124004223ad1957c33afd4effa34d8b8
wuzhixuan <jackwu@java.com>
2020-12-20 19:19:42 +0800

    修改返回结果提

--- src/main/java/com/wuzx/test_pull_image/controller/TestController.java
+++ src/main/java/com/wuzx/test_pull_image/controller/TestController.java
@@ -9,7 +9,12 @@ public class TestController {
 
     @GetMapping("/hello")
     public String getHello() {
-        return "hello3"; 
+        return "猪猪猪猪租啊啊啊啊"; 
+    }
+
+    @GetMapping("/java")
+    public String getJava() {
+        return "java";
     }
         
 }
